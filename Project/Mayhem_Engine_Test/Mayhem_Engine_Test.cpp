ed5cac6348882f1efa83c0d0fd477ef02fc60c1e
Cameron Myers <45009513+cameron-myers@users.noreply.github.com>
2024-04-23 01:36:58 -0700

    Added more test framework for Animation Component

--- Project/Mayhem_Engine_Test/Mayhem_Engine_Test.cpp
+++ Project/Mayhem_Engine_Test/Mayhem_Engine_Test.cpp
@@ -11,25 +11,60 @@ namespace MayhemEngineTest
 {
 	TEST_CLASS(AnimationComponent)
 	{
+
+	private:
+		GameObjectPtr game_object = new GameObject();
+		const wchar_t* to_wchar(const std::wstring in)
+		{
+			return in.c_str();
+		}
 	public:
-		
+
+		TEST_METHOD(ReadTest)
+		{
+
+		}
+
 		TEST_METHOD(AnimationAdvanceFrameTest)
 		{
-			GameObjectPtr obj = new GameObject();
 
 			AnimationPtr animation = new Animation();
 			SpritePtr sprite = new Sprite();
 
-			obj->Add(animation);
-			obj->Add(sprite);
+			game_object->Add(animation);
+			game_object->Add(sprite);
 
 			animation->SetFrameCount(3);
 			int curr_frame = animation->getFrameIndex();
 			animation->AnimationAdvanceFrame();
 			int post_frame = animation->getFrameIndex();
 
-			Assert::IsTrue(post_frame == curr_frame + 1, L"Test");
-			delete obj;
+			Assert::IsTrue(post_frame == curr_frame + 1, to_wchar(L"Animation Frame did not advance! frame was: "
+																			+ std::to_wstring(post_frame) + L"should have been: " + std::to_wstring(curr_frame + 1)));
+		}
+
+
+
+		TEST_METHOD(SetRowForMultiAnimationTest)
+		{
+			
+		}
+		TEST_METHOD(AnimationPlayTest)
+		{
+			
+		}
+		TEST_METHOD(UpdateTest)
+		{
+			
+		}
+		TEST_METHOD(GetRowOfCurrentIndexTest)
+		{
+			
+		}
+
+		TEST_METHOD(SaveTest)
+		{
+			
 		}
 	};
 }
