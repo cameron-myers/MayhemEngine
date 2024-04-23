e7c135618efc8f11a6181ab0fc82d4cc502914e0
Cameron Myers <45009513+cameron-myers@users.noreply.github.com>
2024-04-22 20:36:32 -0700

    build.bat change

--- Project/build.bat
+++ Project/build.bat
@@ -1,7 +1,4 @@
-:echo off
-echo.
 echo. [Mayhem_Engine Build Script]
 cd .\Mayhem_Engine
 
 "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe" "Mayhem_Engine.vcxproj" /build
-
