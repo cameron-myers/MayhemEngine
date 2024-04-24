0d37d75e79ec95c2ba12b03ca5090f5ef1e2769b
Cameron Myers <45009513+cameron-myers@users.noreply.github.com>
2024-04-24 01:30:46 -0700

    Updated test.bat to export result to a file

--- Project/test.bat
+++ Project/test.bat
@@ -1,2 +1,2 @@
-"%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "x64\Debug\Mayhem_Engine_Test.dll"
+"%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "x64\Debug\Mayhem_Engine_Test.dll" /Logger:trx /ResultsDirectory:.\Results"
 pause
