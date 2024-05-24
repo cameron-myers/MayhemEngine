be2e2eb8037af67b2e3489aba2e068f92a4976e1
Cameron Myers <45009513+cameron-myers@users.noreply.github.com>
2024-05-23 21:59:24 -0700

    update functest.bat

--- Project/functest.bat
+++ Project/functest.bat
@@ -1,2 +1,15 @@
 cd Mayhem_Engine
-START /WAIT Mayhem_Engine_x64_Debug.exe /FuncTesting 1
+START "" "Mayhem_Engine_x64_Debug.exe"
+
+:LOOP
+PSLIST Mayhem_Engine_x64_Debug >nul 2>&1
+IF ERRORLEVEL 1 (
+  GOTO CONTINUE
+) ELSE (
+  ECHO Test is still running
+  TIMEOUT /T 5
+  GOTO LOOP
+)
+
+:CONTINUE
+MAYHEM_ENGINE
