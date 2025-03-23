372890f3f42056d70f3d658ffdba9471fb47208a
Cameron Myers <45009513+cameron-myers@users.noreply.github.com>
2025-03-22 19:38:09 -0700

    changed git env name in package script

--- Project/package.bat
+++ Project/package.bat
@@ -1,3 +1,3 @@
-for /f %%i in ('git rev-parse HEAD') do set commit_sha=%%i
+for /f %%i in ('git rev-parse HEAD') do set GIT_COMMIT=%%i
 
-7z a -mmt=on ZeppelinRush_%commit_sha%.zip Assets Mayhem_Engine Mayhem_Wwise Libraries *.dll *.bat
+7z a -mmt=on ZeppelinRush_%GIT_COMMIT%.zip Assets Mayhem_Engine Mayhem_Wwise Libraries *.dll *.bat
